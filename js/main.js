82cd10aca4cb025e218269477b8b2884cdfb66cc
nearjay06 <nearjoan2018@gmail.com>
2018-11-13 15:11:49 +0300

    modify html and css
    [#161645295]

--- js/main.js
+++ js/main.js
@@ -9,4 +9,18 @@ else if(a == false){
 
 }
 
+};
+
+function login(){
+ var a= document.getElementById('admin').checked;
+
+if (a == true){
+ return document.getElementById("choose").setAttribute("action","../pages/admin.html");
 }
+else if(a == false){
+ return document.getElementById("choose").setAttribute("action","../pages/profile.html");
+
+}
+ 
+ }
+ 
